3ad6c439477d65cde7be3f5f78020be917332db3
JohnsonHuang1982 <48971954+JohnsonHuang1982@users.noreply.github.com>
2024-03-18 02:49:57 +0800

    Add files via upload

---
+++ src/main/java/com/zhenio/xreading/model/CurrencyData.java
@@ -0,0 +1,20 @@
+package com.zhenio.xreading.model;
+
+public class CurrencyData {
+	 private String ConDate;
+	 private String Content;
+	 
+	public String getConDate() {
+		return ConDate;
+	}
+	public void setConDate(String conDate) {
+		ConDate = conDate;
+	}
+	public String getContent() {
+		return Content;
+	}
+	public void setContent(String content) {
+		Content = content;
+	}
+	 
+}
